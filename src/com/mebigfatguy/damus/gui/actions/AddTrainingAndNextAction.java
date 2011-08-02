80937174f8e8eb0d9c6b48afcb7af2372bb9b9fe
Dave Brosius <dbrosius@mebigfatguy.com>
2011-08-02 17:41:14 -0400

    fb: BAS

--- src/com/mebigfatguy/damus/gui/actions/AddTrainingAndNextAction.java
+++ src/com/mebigfatguy/damus/gui/actions/AddTrainingAndNextAction.java
@@ -2,17 +2,17 @@
  * damus - a forecasting tool
  * Copyright (C) 2010 Dave Brosius
  * 
- * Licensed under the Apache License, Version 2.0 (the "License"); 
- * you may not use this file except in compliance with the License. 
- * You may obtain a copy of the License at 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  * 
- *    http://www.apache.org/licenses/LICENSE-2.0 
- *    
- * Unless required by applicable law or agreed to in writing, 
- * software distributed under the License is distributed on an "AS IS" BASIS, 
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
- * See the License for the specific language governing permissions and limitations 
- * under the License. 
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations
+ * under the License.
  */
 package com.mebigfatguy.damus.gui.actions;
 
@@ -29,26 +29,26 @@ import com.mebigfatguy.damus.main.Context;
 
 public class AddTrainingAndNextAction extends AbstractAction {
 
-	private static final long serialVersionUID = 3880351786349958924L;
-	private final PanelViewer panelViewer;
-	private final TrainingPanel trainingPanel;
+    private static final long serialVersionUID = 3880351786349958924L;
+    private final PanelViewer panelViewer;
+    private final TrainingPanel trainingPanel;
 
-	public AddTrainingAndNextAction(PanelViewer viewer, TrainingPanel panel) {
-		super(DamusBundle.getString(DamusBundle.ADD_TRAINING_NEXT));
-		panelViewer = viewer;
-		trainingPanel = panel;
-	}
-	
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		Context context = Context.instance();
-		TrainingData data = Context.instance().getTrainingData();
-		if (trainingPanel.populateTrainingItem(data.getItem(data.getNumItems() - 1))) {
-			context.setDirty(true);
-			data.addItem(new TrainingItem());
-			data.setCurrentIndex(data.getNumItems() - 1);
-			trainingPanel.reset();
-		}
-	}
+    public AddTrainingAndNextAction(PanelViewer viewer, TrainingPanel panel) {
+        super(DamusBundle.getString(DamusBundle.ADD_TRAINING_NEXT));
+        panelViewer = viewer;
+        trainingPanel = panel;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        Context context = Context.instance();
+        TrainingData data = context.getTrainingData();
+        if (trainingPanel.populateTrainingItem(data.getItem(data.getNumItems() - 1))) {
+            context.setDirty(true);
+            data.addItem(new TrainingItem());
+            data.setCurrentIndex(data.getNumItems() - 1);
+            trainingPanel.reset();
+        }
+    }
 
 }
